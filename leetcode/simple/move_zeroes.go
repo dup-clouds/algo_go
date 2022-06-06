dfb578d58ac410caffd07f1653d72b6e37e2c0a5
clouds <lpspzsy@gmail.com>
2022-06-06 23:52:18 +0800

    chore():移动零优化

--- leetcode/simple/move_zeroes.go
+++ leetcode/simple/move_zeroes.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println()
 	a := []int{0, 1, 0, 3, 12}
-	moveZeroes(a)
+	moveZeroesLast(a)
 	fmt.Println(a)
 }
 
@@ -15,18 +15,29 @@ func main() {
 // @link https://leetcode.cn/problems/move-zeroes/
 func moveZeroes(nums []int) {
 	n := len(nums)
-	count := 0
 	j := 0
 	for i := 0; i < n; i++ {
 		value := nums[i]
-		if value == 0 {
-			count++
-		} else {
+		if value != 0 {
 			nums[j] = nums[i]
 			j++
 		}
 	}
-	for i := n - count; i < n; i++ {
+	for i := j; i < n; i++ {
 		nums[i] = 0
+	}
+}
+
+// 优化-一次编辑即可，参考快排已排序区间和未排序区间
+func moveZeroesLast(nums []int) {
+	n := len(nums)
+	j := 0
+	for i := 0; i < n; i++ {
+		if nums[i] != 0 {
+			if i > j {
+				nums[j], nums[i] = nums[i], nums[j]
+			}
+			j++
+		}
 	}
 }
