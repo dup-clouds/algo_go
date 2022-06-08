5aceb220dd4324c7c00d4a84a19f1c0500ecabcf
clouds <lpspzsy@gmail.com>
2022-06-09 00:04:05 +0800

    feat():增加删除链表节点双指针实现

--- leetcode/medium/remove_Nth_From_End.go
+++ leetcode/medium/remove_Nth_From_End.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	head := ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4, Next: &ListNode{Val: 5, Next: nil}}}}}
 	fmt.Println()
-	newHead := removeNthFromEnd(&head, 5)
+	newHead := removeNthFromEndLast(&head, 1)
 	curr := newHead
 	for curr != nil {
 		fmt.Println(curr.Val)
@@ -53,4 +53,26 @@ func removeNthFromEnd(head *ListNode, n int) *ListNode {
 	}
 	pre.Next = pre.Next.Next
 	return head
+}
+
+func removeNthFromEndLast(head *ListNode, n int) *ListNode {
+	// p-变量，指针变量指向的内存地址 &p-编译器为变量p分配的内存地址 *p-p指针指向的内容
+	dummy := &ListNode{0, head}
+	// 第一个首节点
+	first := head
+	// 第二个首节点
+	second := dummy
+	// 遍历第一个首节点n次 则剩余可遍历次数为len-n次
+	for i := 0; i < n; i++ {
+		first = first.Next
+	}
+	// 遍历首节点到最后一个 并且移动第二个首节点，当首节点为nil时则说明找到需要删除元素的前一个节点
+	for first != nil {
+		first = first.Next
+		second = second.Next
+	}
+	// 删除元素
+	second.Next = second.Next.Next
+	// 返回节点
+	return dummy.Next
 }
