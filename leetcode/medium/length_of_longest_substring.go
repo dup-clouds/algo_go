007d15cfdd50d9874083bd0d2857e548f0be5f7d
clouds <lpspzsy@gmail.com>
2022-06-12 19:07:43 +0800

    chore():无重复字符串的最长子串多方案实现

--- leetcode/medium/length_of_longest_substring.go
+++ leetcode/medium/length_of_longest_substring.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Println()
-	fmt.Println(lengthOfLongestSubstringLast("bbtablud"))
+	fmt.Println(maxSubStringLen("cada"))
 }
 
 /**
@@ -76,9 +76,69 @@ func lengthOfLongestSubstringLast(s string) int {
 	return maxCount
 }
 
+/**
+r从0开始版本实现
+*/
+func lengthOfLongestSubstringLastCopy(s string) int {
+	// 散列表 存储当前枚举值
+	m := make(map[byte]int)
+	//
+	r, maxCount := 0, 0
+	// 滑动窗口起始位置
+	for i := 0; i < len(s); i++ {
+		// 删除窗口左边的元素，每次i+1则从头开始删除一个
+		if i != 0 {
+			delete(m, s[i-1])
+		} else {
+			m[s[0]]++
+			r++
+		}
+		// 右指针小于s长度且对应s在map中无值时
+		for r < len(s) && m[s[r]] < 1 {
+			m[s[r]]++
+			r++
+		}
+		// 如果r已将整个串遍历完成并放入map中，则判断最大长度与当前i到len的长度，如果小于max，则无需后续滑动遍历
+		if r+1 == len(s) && r-i+1 < maxCount {
+			break
+		}
+		maxCount = max(maxCount, r-i)
+	}
+	return maxCount
+}
+
 func max(x, y int) int {
 	if x > y {
 		return x
 	}
 	return y
+}
+
+/**
+利用双子针完成无重复字符串的最大长度查找
+1. 设置right，left初始值
+2. 初始化窗口
+3. 循环right->n 每次循环将maxVal与right-left进行取最大值
+4. 存在重复元素时left向右移动一位
+*/
+func maxSubStringLen(s string) int {
+	n := len(s)
+	if n < 2 {
+		return n
+	}
+	right := 0
+	left := 0
+	maxVal := 0
+	window := make(map[byte]int)
+	for right < n {
+		c := s[right]
+		window[c]++
+		right++
+		for window[c] > 1 {
+			window[s[left]]--
+			left++
+		}
+		maxVal = max(maxVal, right-left)
+	}
+	return maxVal
 }
