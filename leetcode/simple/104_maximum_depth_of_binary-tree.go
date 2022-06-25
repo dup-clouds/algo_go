35d3123703ba28d1eae804baa39f3addd13952b6
clouds <lpspzsy@gmail.com>
2022-06-25 15:59:37 +0800

    feat():计算二叉树深度

---
+++ leetcode/simple/104_maximum_depth_of_binary-tree.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func main() {
+	left := TreeNode1{9, nil, nil}
+	right := TreeNode1{20, &TreeNode1{15, nil, nil}, &TreeNode1{7, nil, nil}}
+	root := &TreeNode1{3, &left, &right}
+	fmt.Println(maxDepth(root))
+}
+
+type TreeNode1 struct {
+	Val   int
+	Left  *TreeNode1
+	Right *TreeNode1
+}
+
+/**
+@link https://leetcode.cn/problems/maximum-depth-of-binary-tree/
+求解二叉树的最大深度
+*/
+func maxDepth(root *TreeNode1) int {
+	max := 0
+	dfs(root, 0, &max)
+	return max
+}
+
+func dfs(root *TreeNode1, depth int, max *int) {
+	if nil == root {
+		*max = maxVal(depth, *max)
+		return
+	}
+	depth++
+	dfs(root.Left, depth, max)
+	dfs(root.Right, depth, max)
+}
+
+func maxVal(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
