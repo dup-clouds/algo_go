223319d29712753710b2129a934f3cbdca0ee62b
clouds <lpspzsy@gmail.com>
2022-06-29 23:48:46 +0800

    feat():二叉树路径总和新增解法

--- leetcode/medium/113_path_sum_ii.go
+++ leetcode/medium/113_path_sum_ii.go
@@ -6,7 +6,7 @@ func main() {
 	left := TreeNode5{9, nil, nil}
 	right := TreeNode5{20, &TreeNode5{15, nil, nil}, &TreeNode5{7, nil, nil}}
 	root := &TreeNode5{3, &left, &right}
-	fmt.Println(pathSum(root, 12))
+	fmt.Println(pathSum1(root, 12))
 }
 
 type TreeNode5 struct {
@@ -57,5 +57,27 @@ func pathSum(root *TreeNode5, targetSum int) [][]int {
 	var tempPath []int
 	// 一次调用
 	pathFunc(root, &tempPath)
+	return ans
+}
+func pathSum1(root *TreeNode5, targetSum int) [][]int {
+	var ans [][]int
+	var pathFunc func(*TreeNode5, *[]int, int)
+	pathFunc = func(node *TreeNode5, path *[]int, sum int) {
+		if nil == node {
+			return
+		}
+		*path = append(*path, node.Val)
+		sum -= node.Val
+		if node.Left == nil && node.Right == nil && sum == 0 {
+			temp := make([]int, len(*path))
+			copy(temp, *path)
+			ans = append(ans, temp)
+		}
+		pathFunc(node.Left, path, sum)
+		pathFunc(node.Right, path, sum)
+		*path = (*path)[:len(*path)-1]
+	}
+	var tempPath []int
+	pathFunc(root, &tempPath, targetSum)
 	return ans
 }
