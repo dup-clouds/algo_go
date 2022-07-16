9625bfeeafecfcb35f6a94ba3b1a1fafd866eda6
clouds <lpspzsy@gmail.com>
2022-07-16 23:31:59 +0800

    feat():167-求解两数之和增加二分查找+双指针实现

--- leetcode/medium/167_two_sum_ii_input_array_is_sorted.go
+++ leetcode/medium/167_two_sum_ii_input_array_is_sorted.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func main() {
 	fmt.Println()
-	a := []int{-1, 0}
-	fmt.Println(twoSum(a, -1))
+	a := []int{2, 7, 11, 15, 9}
+	fmt.Println(twoSum4(a, 9))
 }
 
 // @link https://leetcode.cn/problems/two-sum-ii-input-array-is-sorted/
@@ -23,6 +23,63 @@ func twoSum(numbers []int, target int) []int {
 				}
 				return []int{j + 1, i + 1}
 			}
+		}
+	}
+	return []int{0, 0}
+}
+
+// 暴力求解，从头开始，时间复杂度O(N)
+func twoSum2(numbers []int, target int) []int {
+	for i := 0; i < len(numbers); i++ {
+		for j := i + 1; j < len(numbers); j++ {
+			if numbers[i]+numbers[j] == target {
+				return []int{i + 1, j + 1}
+			}
+			if numbers[i]+numbers[j] > target {
+				continue
+			}
+		}
+	}
+	return []int{0, 0}
+}
+
+// 遍历+二分查找，固定第一个数，使用二分查找寻找第二个数，时间复杂度O(Nlog(N))
+func twoSum3(numbers []int, target int) []int {
+	// 从0开始选择left元素
+	for i := 0; i < len(numbers); i++ {
+		// 求出right元素
+		rightVal := target - numbers[i]
+		// 二分查找低位
+		low := i + 1
+		// 二分查找高位
+		high := len(numbers) - 1
+		// 循环二分查找
+		for low <= high {
+			// 取中间值，需要加上基础变量low
+			mid := (high-low)/2 + low
+			if rightVal == numbers[mid] {
+				return []int{i + 1, mid + 1}
+			} else if rightVal > numbers[mid] {
+				low = mid + 1
+			} else {
+				high = mid - 1
+			}
+		}
+	}
+	return []int{0, 0}
+}
+
+// 双指针实现，取第一个元素和最后一个元素，依次向中间靠近，遍历完成数组即可找到对应下标，时间复杂度O(N)
+func twoSum4(numbers []int, target int) []int {
+	low := 0
+	high := len(numbers) - 1
+	for low < high {
+		if numbers[low]+numbers[high] == target {
+			return []int{low + 1, high + 1}
+		} else if numbers[low]+numbers[high] < target {
+			low++
+		} else {
+			high--
 		}
 	}
 	return []int{0, 0}
