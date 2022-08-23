c32ab92112625c97f2d533d1ed517d130448ffaf
clouds <lpspzsy@gmail.com>
2022-08-23 23:18:01 +0800

    feat():912-增加快速排序实现排序无序数组

--- leetcode/medium/912_sort_an_array/solution.go
+++ leetcode/medium/912_sort_an_array/solution.go
@@ -51,3 +51,31 @@ func merge(nums []int, low, mid, high int) {
 		nums[m+low] = temp[m]
 	}
 }
+
+// 快速排序
+func sortArray2(nums []int) []int {
+	quicksort(nums, 0, len(nums)-1)
+	return nums
+}
+func quicksort(nums []int, low, high int) {
+	if low >= high {
+		return
+	}
+	p := partition(nums, low, high)
+	quicksort(nums, low, p-1)
+	quicksort(nums, p+1, high)
+}
+func partition(nums []int, low, high int) int {
+	partitionVal := nums[high]
+	i := low
+	for j := low; j < high; j++ {
+		if nums[j] < partitionVal {
+			if i != j {
+				nums[i], nums[j] = nums[j], nums[i]
+			}
+			i++
+		}
+	}
+	nums[i], nums[high] = nums[high], nums[i]
+	return i
+}
--- leetcode/medium/912_sort_an_array/solution_test.go
+++ leetcode/medium/912_sort_an_array/solution_test.go
@@ -10,3 +10,9 @@ func TestSortArray(t *testing.T) {
 	ast.Equal([]int{1, 2, 3, 5}, sortArray([]int{5, 2, 3, 1}))
 	ast.Equal([]int{0, 0, 1, 1, 2, 5}, sortArray([]int{5, 1, 1, 2, 0, 0}))
 }
+
+func TestSortArray2(t *testing.T) {
+	ast := assert.New(t)
+	ast.Equal([]int{1, 2, 3, 5}, sortArray2([]int{5, 2, 3, 1}))
+	ast.Equal([]int{0, 0, 1, 1, 2, 5}, sortArray2([]int{5, 1, 1, 2, 0, 0}))
+}
